5fcdebb75a53da1530e2a4e2a32ddbcd7a1885e7
zzc <370531378@qq.com>
2019-02-11 15:52:31 +0800

    初始化，完成axios获取数据

---
+++ demo-douban/src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '还未获取数据',
+      urlDouban:'https://api.douban.com/v2/movie/top250',
+      urlCnode:'https://cnodejs.org/api/v1/topics'
+    }
+  },
+  created(){
+      this.getData()
+  },
+  methods: {
+    getData() {
+      this.$axios.get('/api/v2/movie/top250',
+      {params:{
+          start:20,
+          count:20
+        }}).then((response) => {
+        console.log(response.data)
+        this.msg=JSON.stringify(response.data.start)
+      })
+    }
+  }
+  //   async getData() {
+  //     try {
+  //       this.msg = await this.$axios.get(this.urlDouban)
+  //     } catch (e) {
+  //       console.log(e.response) // undefined
+  //     }
+  //   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ demo-douban/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios' //引入axios
+
+Vue.prototype.$axios = axios; //添加axios到Vue的原型对象上
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
